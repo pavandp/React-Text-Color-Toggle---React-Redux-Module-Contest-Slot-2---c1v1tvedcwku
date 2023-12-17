3a2b7d510f0eb7dc80b55176f7d77aa53e2966fa
pavankdp <pavankdp29@gmail.com>
2023-12-17 17:31:02 +0530

    color updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,20 @@ import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
-//code here 
+  const [color,setColor]=useState('redColor');
+
+  const handleColor=()=>{
+    if(color==='redColor'){
+      setColor('blueColor');
+    }
+    else{
+      setColor('redColor');
+    }
+  }
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={color} >Newton School</p>
+      <button id='button' onClick={handleColor}>Change Style</button>
     </div>
   )
 }
